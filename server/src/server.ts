d26bfee3ce7653755e888f6144f69b8e55b4d6a9
Rudney Rodrigues <rudney.un2016@gmail.com>
2023-05-23 10:15:02 -0300

    :wrench: chore: Configurando o Prisma + SQLite

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const PORT = 3333
-
 const app = fastify()
+const prisma = new PrismaClient()
 
 app
-  .get('/hello', (reply) => {
-    return 'Hello World'
+  .get('/users', async () => {
+    const users = await prisma.user.findMany()
+
+    return users
   })
   .listen({ port: PORT })
   .then(() => console.log(`🚀 HTTP server running on http://localhost:${PORT}`))
